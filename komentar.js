e4fee07500860c297d50db25d5c8f7d8034bb38e
Khoirul75 <khoirul300@gmail.com>
2023-04-12 09:51:05 +0700

    modul3

---
+++ komentar.js
@@ -0,0 +1,5 @@
+// Merupakan komentar satu baris
+
+/* Merupakan komentar dengan lebih dari satu baris.
+ texs apapun yang berada disini akan dijadikan komentar.
+ */
---
+++ var-let-const.js
@@ -0,0 +1,14 @@
+
+// variabel var
+var nama = "Khoirul Anam", kelas = "RPL A";
+console.log(nama);
+console.log(kelas);
+
+// variabel let
+let firstname;
+ firstname = "Khoirul";
+ console.log(firstname);
+
+ // variabel const
+ const a = 20,b = 30;
+ console.log(a+b);
